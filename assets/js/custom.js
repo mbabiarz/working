cebf07a35cb435edb026a80a287483cf2e6cac05
Margaret Babiarz <sspole@gmail.com>
2015-11-18 20:14:38 -0700

    fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
-//***** Smooth page up *****//
+//***** Smooth page to Top *****//
 $(document).ready(function(){
   // Add smooth scrolling to all links in navbar + footer link
   $("a[href='#top']").on('click', function(event) {
@@ -13,6 +13,29 @@ $(document).ready(function(){
   // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
   $('html, body').animate({
     scrollTop: $(hash).offset().top
+  }, 900, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+    });
+  });
+})
+
+//***** Smooth page to Rotary *****//
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $("a[href='#rotary']").on('click', function(event) {
+
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top - 88
   }, 900, function(){
 
     // Add hash (#) to URL when done scrolling (default click behavior)
