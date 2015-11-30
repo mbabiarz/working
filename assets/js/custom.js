6f2d9df11618389674e5b03ea8e17f38f12237c8
Margaret Babiarz <sspole@gmail.com>
2015-11-29 20:54:50 -0700

    pause at contact, show Randy

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,6 +44,28 @@ $(document).ready(function(){
   });
 })
 
+//***** Smooth page to dealers by region *****//
+$(document).ready(function(){
+  // Add smooth scrolling to all links in navbar + footer link
+  $("a.region").on('click', function(event) {
+
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top - 100
+  }, 900, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+    });
+  });
+})
 //***** Animate home page CTA's *****//
 var pointsArray = document.getElementsByClassName("point");
 
