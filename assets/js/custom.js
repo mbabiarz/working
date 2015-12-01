71f598b554929f9490ccba4009cf0f44427289cd
Margaret Babiarz <sspole@gmail.com>
2015-11-30 18:34:47 -0700

    Misc fixes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -65,6 +65,23 @@ $(document).ready(function(){
     window.location.hash = hash;
     });
   });
+  
+//***** Info table filter *****//
+  (function ($) {
+
+          $('#filter').keyup(function () {
+
+              var rex = new RegExp($(this).val(), 'i');
+              $('.searchable tr').hide();
+              $('.searchable tr').filter(function () {
+                  return rex.test($(this).text());
+              }).show();
+
+          })
+
+      }(jQuery));
+  
+  
 })
 //***** Animate home page CTA's *****//
 var pointsArray = document.getElementsByClassName("point");
@@ -101,32 +118,4 @@ window.onload = function() {
 //***** Master Slider *****//
 jQuery(document).ready(function() {    
   MasterSliderShowcase2.initMasterSliderShowcase2();
-});
-
-//***** Info Table Search *****//
-$(document).ready(function() {
-  $(".search").keyup(function () {
-    var searchTerm = $(".search").val();
-    var listItem = $('.results tbody').children('tr');
-    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
-
-  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
-        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
-    }
-  });
-
-  $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
-    $(this).attr('visible','false');
-  });
-
-  $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
-    $(this).attr('visible','true');
-  });
-
-  var jobCount = $('.results tbody tr[visible="true"]').length;
-    $('.counter').text(jobCount + ' item');
-
-  if(jobCount == '0') {$('.no-result').show();}
-    else {$('.no-result').hide();}
-          });
 });
