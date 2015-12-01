a757e3514baacc2e8adff30bd178ae1ee92dbb3d
Margaret Babiarz <sspole@gmail.com>
2015-11-30 17:40:08 -0700

    New home row

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -102,3 +102,31 @@ window.onload = function() {
 jQuery(document).ready(function() {    
   MasterSliderShowcase2.initMasterSliderShowcase2();
 });
+
+//***** Info Table Search *****//
+$(document).ready(function() {
+  $(".search").keyup(function () {
+    var searchTerm = $(".search").val();
+    var listItem = $('.results tbody').children('tr');
+    var searchSplit = searchTerm.replace(/ /g, "'):containsi('")
+
+  $.extend($.expr[':'], {'containsi': function(elem, i, match, array){
+        return (elem.textContent || elem.innerText || '').toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
+    }
+  });
+
+  $(".results tbody tr").not(":containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','false');
+  });
+
+  $(".results tbody tr:containsi('" + searchSplit + "')").each(function(e){
+    $(this).attr('visible','true');
+  });
+
+  var jobCount = $('.results tbody tr[visible="true"]').length;
+    $('.counter').text(jobCount + ' item');
+
+  if(jobCount == '0') {$('.no-result').show();}
+    else {$('.no-result').hide();}
+          });
+});
